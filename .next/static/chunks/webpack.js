2e73042dfb084d2f3b2d8090c4312c2270584386
Ruslan Zubenko <zubenkoruslan@gmail.com>
2021-02-11 14:14:50 +0200

    Apollo GraphQL boiler plate set up

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cc3918d15bd2eaee7e6d";
+/******/ 	var hotCurrentHash = "4b8a11337b92f15b40e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
