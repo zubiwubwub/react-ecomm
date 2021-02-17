9208f9e2bbb90bb1ca443ce7829ae4bdf6443c4f
Ruslan Zubenko <zubenkoruslan@gmail.com>
2021-02-17 14:23:29 +0200

    Evict cart items from the cache

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a9e83bf72f95fac729bc";
+/******/ 	var hotCurrentHash = "d36f5cc3e0c071e3ff24";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
