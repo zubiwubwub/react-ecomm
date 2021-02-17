a2e227cc91bec053cec222624e51b5a8c9415dc6
Ruslan Zubenko <zubenkoruslan@gmail.com>
2021-02-17 14:17:08 +0200

    remove from cart button added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e002f9d32646a018ee85";
+/******/ 	var hotCurrentHash = "a9e83bf72f95fac729bc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ components/RemoveFromCart.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  outline: none;
+  &:hover {
+    color: var(--red);
+    cursor: pointer;
+  }
+`;
+
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+    deleteCartItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default function RemoveFromCart({ id }) {
+  const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+    variables: { id },
+  });
+  return (
+    <BigButton
+      onClick={removeFromCart}
+      disabled={loading}
+      type='button'
+      title='Remove This Item From Cart'
+    >
+      &times;
+    </BigButton>
+  );
+}
